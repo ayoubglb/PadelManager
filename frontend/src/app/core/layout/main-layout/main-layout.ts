c405f328eea7fb026df51180f6c1ac76d8aeceb2
Ayoub <ghalebayoub@gmail.com>
2026-05-09 23:22:40 +0200

    add SoldeBadge component in main layout toolbar

--- frontend/src/app/core/layout/main-layout/main-layout.ts
+++ frontend/src/app/core/layout/main-layout/main-layout.ts
@@ -15,6 +15,7 @@ import { map } from 'rxjs';
 import { AuthService } from '../../auth/auth.service';
 import { Role } from '../../api/auth.types';
 import {TransactionService} from '../../api/transaction.service';
+import { SoldeBadge } from '../../../shared/components/solde-badge/solde-badge';
 
 interface NavItem {
   label: string;
@@ -68,6 +69,7 @@ const NAV_ITEMS: NavItem[] = [
     MatButtonModule,
     MatMenuModule,
     MatDividerModule,
+    SoldeBadge
   ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
