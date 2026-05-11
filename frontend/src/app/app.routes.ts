62c25133db879e2d17b7a41cf8e3320ccdedb592
Ayoub <ghalebayoub@gmail.com>
2026-05-11 15:52:43 +0200

    add registration component with subscription selection

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,6 +7,11 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./features/auth/login/login').then((m) => m.Login),
   },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register').then((m) => m.Register),
+  },
 
   {
     path: '',
